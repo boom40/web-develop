a691902823fd70eaf436691f16f8175be7581105
boom40 <kickgames675@gmail.com>
2020-03-06 05:28:19 +0200

    Updates

---
+++ precache-manifest.4e3bd7adf95e7df4f8cfc55405adf00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e213552d482aad28d3257ccb4122498",
+    "url": "/web-develop/index.html"
+  },
+  {
+    "revision": "cd5ac070a30ea9afc73d",
+    "url": "/web-develop/static/css/main.3fe4c243.chunk.css"
+  },
+  {
+    "revision": "0169b16b4cc5e90080eb",
+    "url": "/web-develop/static/js/2.66bd28ff.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/web-develop/static/js/2.66bd28ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5ac070a30ea9afc73d",
+    "url": "/web-develop/static/js/main.4c9d2052.chunk.js"
+  },
+  {
+    "revision": "81c848ba835552b51b84",
+    "url": "/web-develop/static/js/runtime-main.6a571d1f.js"
+  }
+]);
